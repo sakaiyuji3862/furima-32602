1d037045e5ac062b1b27f63d1436b8edf4f40444
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-12-19 19:15:55 +0900

    商品情報ActiveHash作成

---
+++ app/models/item.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+    belongs_to :user
+    has_one_attached :image
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :prefecture
+    belongs_to :shipping_cost
+    belongs_to :shipping_day
+
+    with_options presence: true do
+        validates :name
+        validates :price, format: {with: /\A[0-9]+\z/} #半角数字
+        validates :description
+        validates :status_id, numericality: { other_than: 1 }
+        validates :shipping_cost_id, numericality: { other_than: 1 }
+        validates :shipping_days_id
+        validates :prefecture_id, numericality: { other_than: 1 }
+        validates :category_id, numericality: { other_than: 1 }
+        validates :image
+    end
+end
---
+++ spec/factories/shipping_cost_ids.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :shipping_cost_id do
+    
+  end
+end
