3c0383ac166bd1955890f4f721f467882d3810f6
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-12-23 15:26:22 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-    before_action :set_find, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, only: [:new, :edit]
-    before_action :contributor_confirmation, only: :edit
+    before_action :set_find, except: [:index, :new, :create]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :contributor_confirmation, only: [:edit, :update, :destroy]
    
     def index
         @items = Item.includes(:user).order("created_at DESC")
@@ -35,11 +35,8 @@ class ItemsController < ApplicationController
     end
     
     def destroy
-        if @item.destroy
-            redirect_to root_path
-        else
-            redirect_to root_path
-        end
+        @item.destroy
+        redirect_to root_path
     end
 
     private
