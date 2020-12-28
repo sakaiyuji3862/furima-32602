778d71cb3a40676fee30cf82ba0344e76bed8596
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-12-28 14:01:34 +0900

    クレジット決済実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,46 @@
 class OrdersController < ApplicationController
+    before_action :set_find, only: [:index, :create]
+    before_action :authenticate_user!, only: :index
+    before_action :contributor_confirmation, only: [:index, :create]
     def index
-        # @order = Order.new
-        # @item = @order.item
-        @address = Address.new
-        @item = Item.find(params[:item_id])
+        @order_address = OrderAddress.new
     end
 
     def create
-        @address = Address.create(address_params)
-
+        @order_address = OrderAddress.new(order_address_params)
+        if @order_address.valid?
+           pay_item
+            @order_address.save
+            redirect_to root_path
+        else
+            render action: :index
+        end
     end
 
     private
 
-    def address_params
-        params.require(:address).permit(:post_code, :prefecture_id,
+    def order_address_params
+        params.require(:order_address).permit(:post_code, :prefecture_id,
                                         :city, :address, :building_name,
-                                        :phone_number).merge(order_id: orders.id)
+                                        :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    end
+
+    def pay_item
+        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+        Payjp::Charge.create(
+            amount: @item.price,
+            card: order_address_params[:token],
+            currency: 'jpy'
+        )
+    end
+
+    def set_find
+        @item = Item.find(params[:item_id])
+    end
+
+    def contributor_confirmation
+        if current_user == @item.user
+            redirect_to root_path
+        end
     end
 end
