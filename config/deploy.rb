361ea54f3b873caff282dca9d9c1d521be7a4c2b
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-12-30 21:56:53 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "furima-32602"
-set :repo_url, "git@example.com:sakaiyuji3862/furima-32602.git"
+set :repo_url, "git@github.com:sakaiyuji3862/furima-32602.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
